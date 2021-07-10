97e8a9fea9ebc24edb1c8291feaa203ad887bc78
GKGB1212 <binhkhongg@gmail.com>
2021-07-10 13:41:47 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage1 from './pages/homepage-component/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage1/>
+    </div>
+  );
+}
+
+export default App;
